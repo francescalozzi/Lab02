c9a97eaa53653a302fdcc692a72402e78d2fdd6b
francescalozzi <f.lozzi0405@gmail.com>
2025-10-02 14:37:31 +0200

    Soluzione

--- biblioteca.py
+++ biblioteca.py
@@ -1,21 +1,83 @@
 def carica_da_file(file_path):
     """Carica i libri dal file"""
-    # TODO
+    try:
+        infile = open(file_path, 'r', encoding='utf-8')
+        biblioteca = {}
+        from csv import reader
+        csv_reader = reader(infile)
+        for row in csv_reader:
+            if len(row) < 5:
+                continue
+
+            sezione = int(row[4])
+            # creo il libro in precedenza in modo da usare strip() per eliminare eventuali spazi tra i titoli
+            libro = {'nome_libro': row[0].strip(), 'autore': row[1].strip(), 'data': int(row[2]), 'pagine': int(row[3])}
+
+            if sezione not in biblioteca:
+                biblioteca[sezione] = [libro]
+            else:
+                biblioteca[sezione].append(libro)
+
+        infile.close()
+        return biblioteca
+
+    except FileNotFoundError:
+        print('Errore: file non trovato')
+        return None
 
 
 def aggiungi_libro(biblioteca, titolo, autore, anno, pagine, sezione, file_path):
     """Aggiunge un libro nella biblioteca"""
-    # TODO
+
+    if sezione not in biblioteca:
+        print("Errore: sezione non esistente")
+        return None
+
+    for libro in biblioteca[sezione]:
+        if libro["nome_libro"].lower() == titolo.lower():
+            print("Errore: titolo già presente in questa sezione")
+            return None
+
+    nuovo_libro = {'nome_libro': titolo, 'autore': autore, 'data': anno, 'pagine': pagine}
+
+    biblioteca[sezione].append(nuovo_libro)
+
+    try:
+        # con with non serve che venga scritta la chiusura del file
+        with open(file_path, 'a', encoding='utf-8', newline='') as infile:
+            import csv
+            writer = csv.writer(infile)  # trasforma liste/tuple in righe CSV gestendo virgole, virgolette...
+
+            print(
+                file=infile),  # printa una riga vuota --> ci serve perchè sennò il primo libro aggiunto lo printerebbe sull'ultima riga dove c'è già un libro
+            writer.writerow([titolo, autore, anno, pagine, sezione])
+
+    # writer = csv.writer --> serve per scrivere righe nel file csv --> i dati vengono convertiti in formato csv
+    except FileNotFoundError:
+        print('Error: file not found')
+        return None
+
+    return nuovo_libro
 
 
 def cerca_libro(biblioteca, titolo):
     """Cerca un libro nella biblioteca dato il titolo"""
-    # TODO
+    for sezione, libri in biblioteca.items():
+        for libro in libri:
+            if libro['nome_libro'].lower() == titolo.lower():
+                return f'{libro['nome_libro']}, {libro['autore']}, {libro['data']}, {libro['pagine']}, {sezione}'
+    return None
 
 
 def elenco_libri_sezione_per_titolo(biblioteca, sezione):
     """Ordina i titoli di una data sezione della biblioteca in ordine alfabetico"""
-    # TODO
+    if sezione not in biblioteca:
+        return None
+
+    titoli = []
+    for libro in biblioteca[sezione]:
+        titoli.append(libro['nome_libro'])
+    return sorted(titoli, key=str.lower)  # in questo modo se si aggiunge un titolo con la lettera maiuscola, lo rende minuscolo e c'è un ordinamento corretto
 
 
 def main():
@@ -96,5 +158,3 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-
